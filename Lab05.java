86d487019ca7a152d8249a091d93720e3102320e
travis-wentz <wentz.travis@gmail.com>
2015-11-09 09:35:17 -0700

    Reading the file

--- Lab05.java
+++ Lab05.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * 
  * @author TravisWentz JoeDebruycker
@@ -5,9 +9,18 @@
  */
 public class Lab05 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new FileReader("configuration.txt"));
 
+		//for testing
+		String line;
+		while((line = in.readLine()) != null)
+		{
+		    System.out.println(line);
+		}
+		
+		
+		in.close();
 	}
 
 }
