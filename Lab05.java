9a57ddd4266853a70cc1894a88583c16eafec71e
jdebr <jd.debr@gmail.com>
2015-11-09 18:16:23 -0700

    Use scanner to read configuration.txt

--- Lab05.java
+++ Lab05.java
@@ -1,6 +1,7 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * 
@@ -8,19 +9,72 @@ import java.io.IOException;
  *
  */
 public class Lab05 {
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new FileReader("configuration.txt"));
-
-		//for testing
-		String line;
-		while((line = in.readLine()) != null)
-		{
-		    System.out.println(line);
+	
+	public static int xport, yport, zport;
+	public static ArrayList<Integer> xvec = new ArrayList<Integer>();
+	public static ArrayList<Integer> yvec = new ArrayList<Integer>();
+	public static ArrayList<Integer> zvec = new ArrayList<Integer>();
+	
+	public static void readConfig() throws IOException{
+		Scanner sc = new Scanner(new File("configuration.txt"));
+		if(sc.hasNextInt()){
+			Lab05.xport = sc.nextInt();
+		}
+		if(sc.hasNextInt()){
+			Lab05.yport = sc.nextInt();
+		}
+		if(sc.hasNextInt()){
+			Lab05.zport = sc.nextInt();
+		}
+		if(sc.hasNextInt()){
+			xvec.add(sc.nextInt());
+		}
+		if(sc.hasNextInt()){
+			xvec.add(sc.nextInt());
+		}
+		if(sc.hasNextInt()){
+			xvec.add(sc.nextInt());
+		}
+		if(sc.hasNextInt()){
+			yvec.add(sc.nextInt());
+		}
+		if(sc.hasNextInt()){
+			yvec.add(sc.nextInt());
+		}
+		if(sc.hasNextInt()){
+			yvec.add(sc.nextInt());
+		}
+		if(sc.hasNextInt()){
+			zvec.add(sc.nextInt());
+		}
+		if(sc.hasNextInt()){
+			zvec.add(sc.nextInt());
+		}
+		if(sc.hasNextInt()){
+			zvec.add(sc.nextInt());
 		}
 		
+		sc.close();
+	}
+
+	public static void main(String[] args) throws IOException {
+		readConfig();
 		
-		in.close();
+		DatagramSocket xsocket = null;
+		DatagramSocket ysocket = null;
+		DatagramSocket zsocket = null;
+		
+		try{
+			xsocket = new DatagramSocket(xport);
+			ysocket = new DatagramSocket(yport);
+			zsocket = new DatagramSocket(zport);
+		} catch(Exception e){
+			
+		}
+		
+		System.out.println(xvec);
+		System.out.println(yvec);
+		System.out.println(zvec);
 	}
 
 }
